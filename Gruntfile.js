30c812480613302cb81bea7012dd64c3d897a005
Sean Rodriguez <sean.rodriguez@outlook.com>
2017-05-05 00:44:23 -0400

    Configured grunt to use Uglify, Watch, JSHint, CSSMin, Sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,52 @@
 module.exports = function(grunt) {
+  grunt.initConfig ({
+    uglify: {
+      js: {
+        files: {
+          'build/js/scripts.min.js': ['src/js/scripts.js']
+        }
+      }
+    },
 
-  gruntInitConfig ({
-    //
+    jshint: {
+      all: ['Gruntfile.js', 'src/js/*.js']
+    },
+
+    cssmin: {
+      css: {
+        files: [{
+          expand: true,
+          src: ['src/css/main.css'],
+          dest: 'build/css',
+          ext: '.min.css'
+        }]
+      }
+    },
+
+    sass: {
+      dist: {
+        files: {
+          'src/css/main.css': 'src/css/sass/main.sass'
+        }
+      }
+    },
+
+    watch: {
+      js: {
+        files: ['src/js/*.js'],
+        tasks: ['jshint', 'uglify'],
+      },
+      css: {
+        files: ['src/css/sass/*.sass'],
+        tasks: ['sass', 'cssmin'],
+      },
+    },
   });
 
-  // Load function
-
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.registerTask('default', ['jshint', 'uglify', 'sass', 'cssmin', 'watch']);
 };
